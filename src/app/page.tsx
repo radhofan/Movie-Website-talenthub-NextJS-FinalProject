43cba03e291398a40b3563c1e3693c021c2f5fe9
Radho <radhofanazizi@gmail.com>
2024-05-30 21:52:47 +0700

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,14 @@
 import { useEffect, useState } from 'react';
 import MovieItem from "@/components/movieItem";
 
+interface Movie {
+  id: string;
+  title: string;
+  poster_path: string;
+}
+
 export default function Home() {
-  const [movies, setMovies] = useState([]); // Initialize with an empty array
+  const [movies, setMovies] = useState<Movie[]>([]); // Initialize with an empty array and type it as an array of Movie objects
 
   useEffect(() => {
     const fetchMovies = async () => {
@@ -17,10 +23,10 @@ export default function Home() {
         const movieData = await response.json();
 
         // Extract movie IDs from the JSON data
-        const movieIDs = movieData.map(movie => movie.id);
+        const movieIDs = movieData.map((movie: Movie) => movie.id);
 
         // Fetch movie data for each ID
-        const moviePromises = movieIDs.map(async (id) => {
+        const moviePromises = movieIDs.map(async (id: string) => {
           const movieResponse = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=9c9f1a125598ba38d782b021f2db674d`);
           if (!movieResponse.ok) {
             throw new Error(`Failed to fetch data for movie with ID ${id}`);
@@ -43,7 +49,7 @@ export default function Home() {
     <main className="bg-gray-100 min-h-screen p-8 ml-12">
       <div className="flex justify-center">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {movies.map((movie) => (
+          {movies.map((movie: Movie) => (
             <MovieItem
               key={movie.id}
               id={movie.id}
